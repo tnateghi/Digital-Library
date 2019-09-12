e1672fa3510a5bd1e7432103dcd3efac7fbcd8d9
tohid <nategit@gmail.com>
2019-09-12 22:25:55 +0430

    delete extra images from project

--- resources/views/article/master.blade.php
+++ resources/views/article/master.blade.php
@@ -13,15 +13,15 @@
 
     <!-- Icons -->
     <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
-    <link rel="shortcut icon" href="/blog/img/favicon.png">
-    <link rel="apple-touch-icon" href="/blog/img/icon57.png" sizes="57x57">
-    <link rel="apple-touch-icon" href="/blog/img/icon72.png" sizes="72x72">
-    <link rel="apple-touch-icon" href="/blog/img/icon76.png" sizes="76x76">
-    <link rel="apple-touch-icon" href="/blog/img/icon114.png" sizes="114x114">
-    <link rel="apple-touch-icon" href="/blog/img/icon120.png" sizes="120x120">
-    <link rel="apple-touch-icon" href="/blog/img/icon144.png" sizes="144x144">
-    <link rel="apple-touch-icon" href="/blog/img/icon152.png" sizes="152x152">
-    <link rel="apple-touch-icon" href="/blog/img/icon180.png" sizes="180x180">
+    <link rel="shortcut icon" href="/img/favicon.png">
+    <link rel="apple-touch-icon" href="/img/icon57.png" sizes="57x57">
+    <link rel="apple-touch-icon" href="/img/icon72.png" sizes="72x72">
+    <link rel="apple-touch-icon" href="/img/icon76.png" sizes="76x76">
+    <link rel="apple-touch-icon" href="/img/icon114.png" sizes="114x114">
+    <link rel="apple-touch-icon" href="/img/icon120.png" sizes="120x120">
+    <link rel="apple-touch-icon" href="/img/icon144.png" sizes="144x144">
+    <link rel="apple-touch-icon" href="/img/icon152.png" sizes="152x152">
+    <link rel="apple-touch-icon" href="/img/icon180.png" sizes="180x180">
     <!-- END Icons -->
 
     <!-- Stylesheets -->
